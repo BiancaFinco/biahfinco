a99a2877245fb7fb45db31106dde9ba804bf1572
Maria Beatriz <maria.biasouza@gmail.com>
2021-10-31 18:01:56 -0300

    form

--- js/app.js
+++ js/app.js
@@ -8,6 +8,8 @@ const hamburger_menu = document.querySelector(".hamburger-menu");
 const navbar = document.querySelector("header nav");
 const links = document.querySelectorAll(".links a");
 
+$(".grid").isotope({ filter: '.casamento' });
+
 footer_input.addEventListener("focus", () => {
   footer_input.classList.add("focus");
 });
